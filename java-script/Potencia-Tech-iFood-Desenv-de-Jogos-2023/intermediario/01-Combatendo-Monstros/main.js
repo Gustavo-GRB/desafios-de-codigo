5808e560ff021b04ea0397aec2d3582791a85f88
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-11-05 10:56:41 -0300

    Feito desafio 1, Combatendo Monstros

---
+++ java-script/Potencia-Tech-iFood-Desenv-de-Jogos-2023/intermediario/01-Combatendo-Monstros/main.js
@@ -0,0 +1,24 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+// Lê a jogada do personagem:
+var jogadaPersonagem = parseInt(gets());
+
+// Lê a jogada do monstro:
+var jogadaMonstro = parseInt(gets());
+
+// Criar uma função ou outro conceito de lógica de programação para realizar o combate e retornar o resultado:
+function combate() {
+// Implementa uma estrutura condicional if/else para verificar a jogada do personagem e a jogada do monstro, prossiga: 
+  if (jogadaPersonagem > jogadaMonstro) {
+    return "Você venceu a batalha!";
+  } else if (jogadaMonstro > jogadaPersonagem){
+    return "Você perdeu a batalha!"
+  }else
+    return "Foi um empate!"
+}
+
+// Chama a função para realizar o combate para exibir o resultado:
+var resultado = combate();
+print(resultado);
